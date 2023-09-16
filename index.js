89ba1222341cf6d4364781d98114f8c66fa4dcbb
YtaloWill <ytalowillgloria@gmail.com>
2023-09-15 23:46:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const quotes = require("./quotes")
+
+function getQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+module.exports = getQuote;
+
+if (!module.parent) {
+    console.log(getQuote());
+}
---
+++ quotes.js
@@ -0,0 +1,15 @@
+const quotes = [
+    "tic uon tic brei",
+    "EU DIGO TCHALI VOCES DIZEM BRAU",
+    "Uma vez chorão estava passando com seu skate em frente uma escola e viu uma criança chorando. Comovido, imediatamente ele disse \"tic uon tic brei eu digo tiali vcs dizem brau não deixe que ninguém te diga que você não pode fazer algo\", essa criança era ninguém menos que Albert Einstein.",
+    "SKATE",
+    "EU VO FAZER DE UM JEITO QUE ELA NAO VAI SKATER",
+    "Só os skates sabem",
+    "Cuidado com o destino. Ele brinca com as pessoas",
+    "Sem neurose. Cada um sabe o dom, o tom e o tamanho da dose.",
+    "Hoje um gay vai estragar meu dia.",
+    "Troquei o charlie brown pelo sepultura",
+    "sou o lixo do mundo a sociedade me ignora"
+]
+
+module.exports = quotes
